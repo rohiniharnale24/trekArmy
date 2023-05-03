06c5adf805ceec6ddd335da1cf879a3d0433334f
rohini <harnaler@gmail.com>
2023-05-03 14:03:35 +0530

    remove method from registration.components.ts

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UserArrayService } from '../user-array.service';
-import { Iuser } from '../shared/model/userArray';
 
 @Component({
   selector: 'app-registration',
@@ -8,34 +6,7 @@ import { Iuser } from '../shared/model/userArray';
   styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
-  // userArray: Array<Iuser> = [];
-  constructor(private _userInstance: UserArrayService) {}
-  ngOnInit(): void {
-    // this.userArray = this._userInstance.userArray;
-  }
-  registeruser(
-    fname: HTMLInputElement,
-    lname: HTMLInputElement,
-    email: HTMLInputElement,
-    password: HTMLInputElement,
-    address: HTMLInputElement,
-    city: HTMLInputElement
-  ) {
-    let obj = {
-      fname: fname.value,
-      lname: lname.value,
-      email: email.value,
-      password: password.value,
-      address: address.value,
-      city: city.value,
-    };
-    console.log(obj);
-    this._userInstance.addUser(obj);
-    fname.value = '';
-    lname.value = '';
-    email.value = '';
-    password.value = '';
-    address.value = '';
-    city.value = '';
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
